b224f5c23b27bf3e3d3eff823345b470f9e19396
Nahida kawaii <1756161456@qq.com>
2025-02-11 15:02:43 +0800

    ArrayList-05-集合练习-返回多个数据

---
+++ basic-code/mylist/src/main/java/com/nahida/test/Test8.java
@@ -0,0 +1,29 @@
+package com.nahida.test;
+
+import java.util.ArrayList;
+
+public class Test8 {
+    public static void main(String[] args) {
+        ArrayList<Phone> list = new ArrayList<>();
+        Phone p1 = new Phone("华为", 5999);
+        Phone p2 = new Phone("小米", 1999);
+        Phone p3 = new Phone("三星", 3999);
+        list.add(p1);
+        list.add(p2);
+        list.add(p3);
+        ArrayList<Phone> newList = getPhoneInfo(list);
+        for (Phone p : newList) {
+            System.out.println(p.getBrand() + ":" + p.getPrice());
+        }
+    }
+
+    public static ArrayList<Phone> getPhoneInfo(ArrayList<Phone> list) {
+        ArrayList<Phone> newList = new ArrayList<>();
+        for (Phone p : list) {
+            if (p.getPrice() < 3000) {
+                newList.add(p);
+            
+        }
+        return newList;
+    }
+}
